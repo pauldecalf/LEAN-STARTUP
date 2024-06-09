073bddfa128f4ed761b2d571e6c84053fbc2692d
Navre <pauldecalf@outlook.fr>
2024-06-09 18:50:47 +0200

    Mise ne place du responsive pour la version Desktop

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { join } from 'path';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
+
 @Module({
   imports: [
     ServeStaticModule.forRoot({
